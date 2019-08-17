57434062aca0c43f6b3300cf3673a0fb458f6604
Josefina <josefinaestevez91@gmail.com>
2019-08-17 16:21:00 -0300

    nicetable model

--- backend/infovis/nicetable/models.py
+++ backend/infovis/nicetable/models.py
@@ -1,3 +1,8 @@
 from django.db import models
+from django_mysql.models import JSONField
 
 # Create your models here.
+class NiceTable(models.Model):
+    domain = models.CharField(max_length=100)
+    table_id = models.IntegerField()
+    columns_conf = JSONField()
