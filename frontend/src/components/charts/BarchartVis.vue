c258c8577eccb712e43a4607114e05c3b756f84c
Josefina <josefinaestevez91@gmail.com>
2019-10-11 18:29:02 -0300

    styling

--- frontend/src/components/charts/BarchartVis.vue
+++ frontend/src/components/charts/BarchartVis.vue
@@ -1,29 +1,47 @@
 <template>
   <div style="margin: 50px; 0;">
-     <a-button
-      style="margin-left: 5px; margin-top: 50px; float: left;"
-      type="primary"
-      @click="removeChart()"
-    >
-      Eliminar Grafico
-    </a-button>
-    <div ref="chart" />
+    <a-row>
+      <a-col :span="8" :offset="1">
+        <a-row>
+          <a-button
+            style="float: left; margin-bottom: 50px"
+            type="primary"
+            @click="removeChart()"
+          >Eliminar Grafico</a-button>
+        </a-row>
+        <a-row
+          v-for="(element, index) in sortedData"
+          :key="index"
+          style="text-align: left; margin: 5px 0; font-weight: 700; font-size: 12px;"
+        >
+          <a-col :span="2" class="dot" :style="`background-color: ${element.color}`"></a-col>
+          <a-col :span="20" :offset="1">
+            <p>{{element.legend}}: {{element.value}}</p>
+          </a-col>
+        </a-row>
+      </a-col>
+      <a-col :span="15">
+        <div ref="chart">
+          <h3 style="margin-top: 15px; font-weight: bold;">{{ this.conf.field }}</h3>
+        </div>
+      </a-col>
+    </a-row>
   </div>
 </template>
 <script>
-import * as d3 from 'd3'
-import rendering from '@/utils/rendering'
-import types from '@/utils/types'
-import { Button } from 'ant-design-vue'
+import * as d3 from "d3";
+import rendering from "@/utils/rendering";
+import types from "@/utils/types";
+import { Row, Col, Button } from "ant-design-vue";
 
-const utils = { ...rendering, ...types }
+const utils = { ...rendering, ...types };
 
 export default {
   props: {
     id: {
       type: Number,
-      default: function () {
-        return 0
+      default: function() {
+        return 0;
       }
     },
     niceTable: {
@@ -37,71 +55,100 @@ export default {
   },
 
   components: {
-    'a-button': Button
+    "a-row": Row,
+    "a-col": Col,
+    "a-button": Button
   },
 
   computed: {
-
-    backend () {
-      return this.niceTable.getBackend()
+    backend() {
+      return this.niceTable.getBackend();
     },
 
-    columns () {
-      return this.niceTable.getVisibleColumns()
+    columns() {
+      return this.niceTable.getVisibleColumns();
     },
 
-    rows () {
-      let niceTableRows = this.niceTable.getRows()
-      let selectedRows = this.conf.selectedRows
-      return niceTableRows.filter(row => selectedRows.indexOf(niceTableRows.indexOf(row)) >= 0)
+    rows() {
+      let niceTableRows = this.niceTable.getRows();
+      let selectedRows = this.conf.selectedRows;
+      return niceTableRows.filter(
+        row => selectedRows.indexOf(niceTableRows.indexOf(row)) >= 0
+      );
+    },
+
+    sortedData() {
+      return this._.orderBy(this.data, ["value"], ["desc"]);
     }
   },
 
-  mounted () {
-    let data = this.transformData()
-    this.draw(data)
+  data() {
+    return {
+      data: null
+    };
+  },
+
+  mounted() {
+    this.data = this.transformData();
+    this.draw();
   },
 
   methods: {
-    draw (data) {
-      var margin = {top: 10, right: 10, bottom: 100, left: 60}
+    draw() {
+      var data = this.data;
+      var margin = { top: 10, right: 10, bottom: 100, left: 60 };
       var width = 800 - margin.left - margin.right,
-        height = 600 - margin.top - margin.bottom
+        height = 600 - margin.top - margin.bottom;
 
-      var x = d3.scale.ordinal()
-        .rangeRoundBands([0, width], 1)
+      var x = d3.scale.ordinal().rangeRoundBands([0, width], 1);
 
-      var x2 = d3.scale.ordinal()
-        .rangeRoundBands([0, width], 0)
+      var x2 = d3.scale.ordinal().rangeRoundBands([0, width], 0);
 
-      var y = d3.scale.linear()
-        .range([height,0])
+      var y = d3.scale.linear().range([height, 0]);
 
-      var xAxis = d3.svg.axis()
+      var xAxis = d3.svg
+        .axis()
         .scale(x)
-        .orient("bottom")
+        .orient("bottom");
 
-      var yAxis = d3.svg.axis()
+      var yAxis = d3.svg
+        .axis()
         .scale(y)
-        .orient("left")
+        .orient("left");
 
-      var color = d3.scale.category10()
+      var color = d3.scale.category10();
 
       data.forEach(function(d) {
-        d['value'] = +d['value']
-      })
+        d["value"] = +d["value"];
+      });
 
-      var svg = d3.select(this.$refs.chart).append("svg")
+      var svg = d3
+        .select(this.$refs.chart)
+        .append("svg")
         .attr("width", width + margin.left + margin.right)
         .attr("height", height + margin.top + margin.bottom)
         .append("g")
-        .attr("transform", "translate(" + margin.left + "," + margin.top + ")")
+        .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
 
-      x.domain(data.map(function(d) { return d['legend'] }))
-      y.domain([0, d3.max(data, function(d) { return d['value'] })])
-      x2.domain(data.map(function(d) { return d['legend'] }))
+      x.domain(
+        data.map(function(d) {
+          return d["legend"];
+        })
+      );
+      y.domain([
+        0,
+        d3.max(data, function(d) {
+          return d["value"];
+        })
+      ]);
+      x2.domain(
+        data.map(function(d) {
+          return d["legend"];
+        })
+      );
 
-      svg.append("g")
+      svg
+        .append("g")
         .attr("class", "axis axis--x")
         .attr("transform", "translate(0," + height + ")")
         .call(xAxis)
@@ -110,103 +157,130 @@ export default {
         .attr("x", 9)
         .attr("dy", "-0.2em")
         .attr("transform", "rotate(90)")
-        .style("text-anchor", "start")
+        .style("text-anchor", "start");
 
-      svg.append("g")
+      svg
+        .append("g")
         .attr("class", "axis axis--y")
         .call(yAxis)
         .append("text")
         //.attr("transform", "rotate(-90)")
         .attr("y", 6)
         .attr("dy", "0em")
-        .attr("text-anchor", "end")
+        .attr("text-anchor", "end");
 
-      var bars = svg.append("g").attr("class", "bars")
+      var bars = svg.append("g").attr("class", "bars");
 
-      bars.selectAll(".bar")
+      bars
+        .selectAll(".bar")
         .data(data)
-        .enter().append("rect")
+        .enter()
+        .append("rect")
         .attr("class", "bar")
-        .attr("x", function (d) { return x(d['legend']) })
-        .attr("y", function (d) { return y(d['value']) })
+        .attr("x", function(d) {
+          return x(d["legend"]);
+        })
+        .attr("y", function(d) {
+          return y(d["value"]);
+        })
         .attr("width", 10)
-        .attr("height", function (d) { return height - y(d['value']) })
-        .style("fill", function (d) { return d['color'] })
+        .attr("height", function(d) {
+          return height - y(d["value"]);
+        })
+        .style("fill", function(d) {
+          return d["color"];
+        });
 
-      var sum = d3.sum(data, function(d) { return d['value'] })
-      var average = sum/data.length
+      var sum = d3.sum(data, function(d) {
+        return d["value"];
+      });
+      var average = sum / data.length;
 
-      var line = d3.svg.line()
-        .x(function(d, i) { return x2(d['legend']) + i })
-        .y(function(d, i) { return y(average) })
+      var line = d3.svg
+        .line()
+        .x(function(d, i) {
+          return x2(d["legend"]) + i;
+        })
+        .y(function(d, i) {
+          return y(average);
+        });
 
-      svg.append("path")
+      svg
+        .append("path")
         .datum(data)
         .attr("class", "mean")
-        .attr("d", line)
-
-      svg.append("text")
-        .attr("transform", "translate(" + (width+3) + "," + y(average) + ")")
-        .attr("dy", "1em")
-        .attr("text-anchor", "end")
-        .style("fill", "black")
-        .html(`${this.conf.field}`)
+        .attr("d", line);
     },
 
-    getForm () {
-      const instruction = 'Selecciona un campo'
-      const name = 'Gráfico de barras'
-      let fields = []
+    getForm() {
+      const instruction = "Selecciona un campo";
+      const name = "Gráfico de barras";
+      let fields = [];
       fields.push({
-        name: '',
+        name: "",
         type: [],
-        model: 'field',
+        model: "field",
         required: true,
         max: 1
-      })
+      });
       let form = {
         instruction,
         fields,
         name
-      }
-      return form
+      };
+      return form;
     },
 
     // Processing data
-    transformData () {
-      let chartData = []
-      let selectedColumn = this.columns.find(column => column.dataIndex == this.conf.field)
+    transformData() {
+      let chartData = [];
+      let allColors = utils.colors;
+      let selectedColumn = this.columns.find(
+        column => column.dataIndex == this.conf.field
+      );
+      let color;
       this.rows.forEach(row => {
         if (selectedColumn.type == utils.number) {
-          let value = parseFloat(row[this.conf.field])
+          color =
+            allColors.length >= 1
+              ? allColors.pop()
+              : utils.generateRandomColor();
+          let value = parseFloat(row[this.conf.field]);
           let element = {
             legend: row[this.conf.field],
             value: !isNaN(value) ? value : 0,
-            color: utils.getRandomColor()
-          }
-          chartData.push(element)
+            color
+          };
+          chartData.push(element);
         } else {
-          let columnValue = chartData.length > 0 ? chartData.find(e => e.legend == row[this.conf.field]) : null
+          let columnValue =
+            chartData.length > 0
+              ? chartData.find(e => e.legend == row[this.conf.field])
+              : null;
           if (!columnValue) {
+            color =
+              allColors.length >= 1
+                ? allColors.pop()
+                : utils.generateRandomColor();
             let element = {
               legend: row[this.conf.field],
               value: 1,
-              color: utils.getRandomColor()
-            }
-            chartData.push(element)
+              color
+            };
+            chartData.push(element);
           } else {
-            columnValue.value += 1
+            columnValue.value += 1;
           }
         }
-      })
-      return chartData
+      });
+      return chartData;
     },
 
-    removeChart () {
-      utils.removeChart(this)
+    removeChart() {
+      utils.removeChart(this);
     }
   }
-}
+};
 </script>
 <style scoped>
 .axis line,
@@ -215,20 +289,27 @@ export default {
   stroke: #000;
   shape-rendering: crispEdges;
 }
-.axis--x line{
+.axis--x line {
   display: none;
 }
 .axis--x path {
-    display: none;
+  display: none;
 }
 .axis--y path {
   display: none;
 }
-h1, h3 {
+h1,
+h3 {
   text-align: center;
 }
 .mean {
   stroke-width: 1px;
   stroke: red;
+}
+.dot {
+  height: 25px;
+  width: 25px;
+  border-radius: 50%;
+  display: inline-block;
 }
 </style>
--- frontend/src/components/charts/PiechartVis.vue
+++ frontend/src/components/charts/PiechartVis.vue
@@ -1,28 +1,45 @@
 <template>
   <div style="margin: 50px; 0;">
-    <a-button
-      style="margin-left: 5px; margin-top: 50px; float: left;"
-      type="primary"
-      @click="removeChart()"
-    >
-      Eliminar Grafico
-    </a-button>
-    <h3>{{ this.conf.field }}</h3>
-    <div ref="chart" />
+    <a-row>
+      <a-col :span="8" :offset="1">
+        <a-row>
+          <a-button
+            style="float: left; margin-bottom: 50px"
+            type="primary"
+            @click="removeChart()"
+          >Eliminar Grafico</a-button>
+        </a-row>
+        <a-row
+          v-for="(element, index) in sortedData"
+          :key="index"
+          style="text-align: left; margin: 5px 0; font-weight: 700; font-size: 12px;"
+        >
+          <a-col :span="2" class="dot" :style="`background-color: ${element.color}`"></a-col>
+          <a-col :span="20" :offset="1">
+            <p>{{element.legend}}: {{element.percentage}}</p>
+          </a-col>
+        </a-row>
+      </a-col>
+      <a-col :span="15">
+        <div ref="chart">
+          <h3 style="margin-top: 15px; font-weight: bold;">{{ this.conf.field }}</h3>
+        </div>
+      </a-col>
+    </a-row>
   </div>
 </template>
 
 <script>
-import * as d3 from 'd3'
-import utils from '@/utils/rendering'
-import { Button } from 'ant-design-vue'
+import * as d3 from "d3";
+import utils from "@/utils/rendering";
+import { Row, Col, Button } from "ant-design-vue";
 
 export default {
   props: {
     id: {
       type: Number,
-      default: function () {
-        return 0
+      default: function() {
+        return 0;
       }
     },
     niceTable: {
@@ -36,72 +53,93 @@ export default {
   },
 
   components: {
-    'a-button': Button
+    "a-row": Row,
+    "a-col": Col,
+    "a-button": Button
   },
 
   computed: {
-
-    backend () {
-      return this.niceTable.getBackend()
+    backend() {
+      return this.niceTable.getBackend();
     },
 
-    columns () {
-      return this.niceTable.getVisibleColumns()
+    columns() {
+      return this.niceTable.getVisibleColumns();
     },
 
-    rows () {
-      let niceTableRows = this.niceTable.getRows()
-      let selectedRows = this.conf.selectedRows
-      return niceTableRows.filter(row => selectedRows.indexOf(niceTableRows.indexOf(row)) >= 0)
+    rows() {
+      let niceTableRows = this.niceTable.getRows();
+      let selectedRows = this.conf.selectedRows;
+      return niceTableRows.filter(
+        row => selectedRows.indexOf(niceTableRows.indexOf(row)) >= 0
+      );
+    },
+
+    sortedData() {
+      return this._.orderBy(this.data, ["value"], ["desc"]);
     }
   },
 
-  mounted () {
-    let data = this.transformData()
-    this.draw(data)
+  data() {
+    return {
+      data: null
+    };
+  },
+
+  mounted() {
+    this.data = this.transformData();
+    this.draw();
   },
 
   methods: {
     // Rendering
-    draw (data) {
-      var dataset = data
+    draw() {
+      var dataset = this.data;
 
-      var width = 400
-      var height = 400
-      var radius = 200
+      var width = 500;
+      var height = 500;
+      var radius = 220;
 
-      var total = 0
+      var total = 0;
       dataset.forEach(element => {
-        total += element.value
-      })
+        total += element.value;
+      });
 
-      var svg = d3.select(this.$refs.chart).append("svg")
+      var svg = d3
+        .select(this.$refs.chart)
+        .append("svg")
         .attr("width", width)
         .attr("height", height)
         .append("g")
-        .attr("transform", "translate(" + width / 2 + "," + height / 2 + ")")
+        .attr("transform", "translate(" + width / 2 + "," + height / 2 + ")");
 
-      var arc = d3.svg.arc()
-          .outerRadius(radius)
-          .innerRadius(60)
+      var arc = d3.svg
+        .arc()
+        .outerRadius(radius)
+        .innerRadius(60);
 
-      var pie = d3.layout.pie()
-          .sort(null)
-          .value(function(d){ return d.value })
+      var pie = d3.layout
+        .pie()
+        .sort(null)
+        .value(function(d) {
+          return d.value;
+        });
 
-      var g = svg.selectAll(".fan")
-          .data(pie(dataset))
-          .enter()
-          .append("g")
-          .attr("class", "fan")
+      var g = svg
+        .selectAll(".fan")
+        .data(pie(dataset))
+        .enter()
+        .append("g")
+        .attr("class", "fan");
 
       // Define the div for the tooltip
-      var div = d3.select("body")
+      var div = d3
+        .select(this.$refs.chart)
         .append("div")
         .style("position", "absolute")
         .style("text-align", "center")
-        .style("width", "80px")
-        .style("height", "30px")
+        .style("width", "auto")
+        .style("height", "auto")
         .style("padding", "8px")
         .style("font", "12px sans-serif")
         .style("background-color", "#d1e8e3")
@@ -110,11 +148,13 @@ export default {
         .style("font-weight", "bold")
         .style("border-radius", "8px")
         .style("pointer-events", "none")
-        .style("opacity", 0)
+        .style("opacity", 0);
 
       g.append("path")
         .attr("d", arc)
-        .attr("fill", function(d){ return d.data.color })
+        .attr("fill", function(d) {
+          return d.data.color;
+        })
         .on("mouseover", function(d) {
           div
             .transition()
@@ -131,69 +171,88 @@ export default {
             .duration(500)
             .style("opacity", 0);
         });
-      
-      g.append("text")
-        .attr("transform", function(d) { return "translate(" + arc.centroid(d) + ")" })
-        .style("text-anchor", "middle")
-        .text(function(d) { return `${(d.value / total)*100}%` })
-
     },
 
-    getForm () {
-      const instruction = 'Selecciona un campo'
-      const name = 'Gráfico de torta'
-      let fields = []
+    getForm() {
+      const instruction = "Selecciona un campo";
+      const name = "Gráfico de torta";
+      let fields = [];
       fields.push({
-        name: '',
+        name: "",
         type: [],
-        model: 'field',
+        model: "field",
         required: true,
         max: 1
-      })
+      });
       let form = {
         instruction,
         fields,
         name
-      }
-      return form
+      };
+      return form;
     },
 
     // Processing data
-    transformData () {
-      let chartData = []
-      let selectedColumn = this.columns.find(column => column.dataIndex == this.conf.field)
+    transformData() {
+      let chartData = [];
+      let allColors = utils.colors;
+      let selectedColumn = this.columns.find(
+        column => column.dataIndex == this.conf.field
+      );
+      let color;
       this.rows.forEach(row => {
         if (selectedColumn.type == utils.number) {
-          let value = parseFloat(row[this.conf.field])
+          let value = parseFloat(row[this.conf.field]);
+          color =
+            allColors.length >= 1
+              ? allColors.pop()
+              : utils.generateRandomColor();
           let element = {
             legend: row[this.conf.field],
             value: !isNaN(value) ? value : 0,
-            color: utils.getRandomColor()
-          }
-          chartData.push(element)
+            color
+          };
+          chartData.push(element);
         } else {
-          let columnValue = chartData.length > 0 ? chartData.find(e => e.legend == row[this.conf.field]) : null
+          let columnValue =
+            chartData.length > 0
+              ? chartData.find(e => e.legend == row[this.conf.field])
+              : null;
           if (!columnValue) {
+            color =
+              allColors.length >= 1
+                ? allColors.pop()
+                : utils.generateRandomColor();
             let element = {
               legend: row[this.conf.field],
               value: 1,
-              color: utils.getRandomColor()
-            }
-            chartData.push(element)
+              color
+            };
+            chartData.push(element);
           } else {
-            columnValue.value += 1
+            columnValue.value += 1;
           }
         }
-      })
-      return chartData
+      });
+      let total = chartData.reduce((a, b) => a + b.value, 0);
+      chartData = chartData.map(chartItem => {
+        let percentage = ((chartItem.value / total) * 100).toPrecision(2);
+        return {
+          legend: chartItem.legend,
+          value: chartItem.value,
+          color: chartItem.color,
+          percentage: `${percentage}%`
+        };
+      });
+      return chartData;
     },
 
     // API
-    removeChart () {
-      utils.removeChart(this)
+    removeChart() {
+      utils.removeChart(this);
     }
   }
-}
+};
 </script>
 
 <style scoped>
@@ -204,5 +263,11 @@ export default {
 
 .arc path {
   stroke: #fff;
+}
+.dot {
+  height: 25px;
+  width: 25px;
+  border-radius: 50%;
+  display: inline-block;
 }
 </style>
